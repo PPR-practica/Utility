f485e982262032b85cc630ec466ac225c95e3367
carmen <carmen>
2016-08-18 10:42:52 +0300

    descriere

--- Utility/Utility/MainWindow.xaml.cs
+++ Utility/Utility/MainWindow.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.ComponentModel;
 using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 using Utility.Text_Operations;
 
 namespace Utility
@@ -24,11 +29,22 @@ namespace Utility
         public MainWindow()
         {
             //string input = "Igor;Ivan;Albert;Sofia;Mary;Salmonela;Igor;Igor";
-
             //List<string> output = TextOperations.SplitAndUniquify(input, ";");
+            //InitializeComponent();            
+        }
 
-            InitializeComponent();
-            
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ofd.ShowDialog();
+
+        }
+
+        //start, run, services.msc, TabletInputService
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
         }
     }
 }
